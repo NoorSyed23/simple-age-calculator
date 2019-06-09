594f5abba2a351135c76d16e5b20c451e8cf7ccc
Noor Syed <noorsyed23@gmail.com>
2019-06-09 15:16:50 -0500

    age-calculator

---
+++ app.js
@@ -0,0 +1,7 @@
+var name = prompt('What is your age');
+alert('Hi' + name);
+
+var dob = prompt ('What year were you born?');
+var yearTill = prompt('What year till you want to calculate your age?');
+
+alert(yearTill - dob);
