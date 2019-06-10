a4f9067b7df083051be2c1fe53241f0a75b452da
Noor Syed <noorsyed23@gmail.com>
2019-06-10 14:55:50 -0500

    update message

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var name = prompt('What is your age');
-alert('Hi' + name);
+var name = prompt('What is your name');
+alert('Hi ' + name);
 
 var dob = prompt ('What year were you born?');
 var yearTill = prompt('What year till you want to calculate your age?');
